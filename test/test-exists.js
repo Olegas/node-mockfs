0ef974b6ef6b288eaa655d10c832fbc08d5bc94e
Oleg Elifantiev <oleg@elifantiev.ru>
2013-05-05 22:46:04 +0400

    Basic idea, some skeleton, tests

---
+++ test/test-exists.js
@@ -0,0 +1,55 @@
+var mfs = require('../'),
+    assert = require('assert'),
+    fs = require('fs'),
+    mounted;
+
+describe("exists", function(){
+
+   before(function(){
+      mounted = mfs.mount({
+         items: {
+            'file.txt': '',
+            'dir': {
+               items: {
+                  'file.txt': ''
+               }
+            }
+         }
+      }, '/mnt/mock');
+   });
+
+
+   describe("can check if file exists", function(){
+
+      it("via sync api", function(){
+         assert.equal(true, fs.existsSync('/mnt/mock/file.txt'));
+         assert.equal(true, fs.existsSync('/mnt/mock/dir/file.txt'));
+         assert.equal(false, fs.existsSync('/mnt/mock/no-file.txt'));
+      });
+
+      it("via async api", function(done){
+         fs.exists('/mnt/mock/file.txt', function(r){
+            assert.equal(true, r);
+         });
+         fs.exists('/mnt/mock/dir/file.txt', function(r){
+            assert.equal(true, r);
+         });
+         fs.exists('/mnt/mock/no-file.txt', function(r){
+            assert.equal(false, r);
+            done();
+         });
+      });
+
+   });
+
+   it("can check if directory exists", function(){
+      assert.equal(true, fs.existsSync('/mnt/mock/dir'));
+      assert.equal(false, fs.existsSync('/mnt/mock/dir/no-dir'));
+   });
+
+   after(function(){
+      mounted.umount();
+   });
+
+});
+
---
+++ test/test-readFile.js
@@ -0,0 +1,84 @@
+var mfs = require('../'),
+    assert = require('assert'),
+    fs = require('fs'),
+    mounted;
+
+describe("readFile", function(){
+
+   before(function(){
+      mounted = mfs.mount({
+         items: {
+            'file-buffer': new Buffer('qwerty'),
+            'file-string': 'qwerty',
+            'file-base64': new Buffer('cXdlcnR5', 'base64'),
+            'file-alt': {
+               content: 'asobject'
+            },
+            'dir': {
+               items: {}
+            }
+         }
+      }, '/mnt/mock');
+
+   });
+
+   it("can read file synchronously", function(){
+
+      assert.equal("qwerty", fs.readFileSync('/mnt/mock/file-buffer').toString());
+      assert.equal("qwerty", fs.readFileSync('/mnt/mock/file-string').toString());
+      assert.equal("asobject", fs.readFileSync('/mnt/mock/file-alt').toString());
+      assert.equal("qwerty", fs.readFileSync('/mnt/mock/file-base64').toString());
+      assert.equal("cXdlcnR5", fs.readFileSync('/mnt/mock/file-base64').toString('base64'));
+
+   });
+
+   it("can read file asynchronously", function(){
+
+      /*assert.equal("qwerty", fs.readFileSync('/mnt/mock/file-string').toString());
+      assert.equal("asobject", fs.readFileSync('/mnt/mock/file-alt').toString());
+      assert.equal("qwerty", fs.readFileSync('/mnt/mock/file-base64').toString());
+      assert.equal("cXdlcnR5", fs.readFileSync('/mnt/mock/file-base64').toString('base64'));*/
+
+   });
+
+   it("Calling without callback doesn't throw", function(){
+      assert.doesNotThrow(function(){
+         fs.readFile('/mnt/mock/file-buffer');
+      }, Error);
+   });
+
+   it("Calling without arguments throws a TypeError", function(){
+      assert.throws(function(){
+         fs.readFile();
+      }, TypeError);
+   });
+
+   it("throws EISDIR whe trying to read directory", function(done){
+      assert.throws(function(){
+         fs.readFileSync('/mnt/mock/dir');
+      }, /EISDIR/);
+
+      fs.readFile('/mnt/mock/dir', function(e, r){
+         assert.equal('EISDIR', e.message);
+         assert.equal(undefined, r);
+         done();
+      });
+   });
+
+   it("throws ENOENT if trying to read nonexistent file", function(done){
+      assert.throws(function(){
+         fs.readFileSync('/mnt/mock/nonexist-file');
+      }, /ENOENT/);
+
+      fs.readFile('/mnt/mock/nonexist-file', function(e, r){
+         assert.equal('ENOENT', e.message);
+         assert.equal(undefined, r);
+         done();
+      });
+   });
+
+   after(function(){
+      mounted.umount();
+   });
+});
+
