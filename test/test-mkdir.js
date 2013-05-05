5b99bef3a34a46dad67b17c0d97d2dbc22564e10
Oleg Elifantiev <oleg@elifantiev.ru>
2013-05-06 01:28:41 +0400

    mkdirSync support draft

---
+++ test/test-mkdir.js
@@ -0,0 +1,50 @@
+var mfs = require('../'),
+   assert = require('assert'),
+   fs = require('fs'),
+   mounted;
+
+describe("mkdir", function(){
+
+   before(function(){
+      mounted = mfs.mount({
+         items: {
+            'file': '',
+            'adir': {
+               items: {}
+            }
+         }
+      }, '/mnt/mock');
+
+   });
+
+   it("can create directories in already existing ones", function(){
+
+      assert.equal(false, fs.existsSync('/mnt/mock/adir/some'));
+      fs.mkdirSync('/mnt/mock/adir/some');
+      assert.equal(true, fs.existsSync('/mnt/mock/adir/some'));
+
+   });
+
+   it("throws ENOTDIR whe parent is a file", function(){
+      assert.throws(function(){
+         fs.mkdirSync('/mnt/mock/file/some');
+      }, /ENOTDIR/);
+   });
+
+   it("throws ENOENT if parent is not exists", function(){
+      assert.throws(function(){
+         fs.mkdirSync('/mnt/mock/nonexist/some');
+      }, /ENOENT/);
+   });
+
+   it("throws EEXIST if directory is already exists", function(){
+      assert.throws(function(){
+         fs.mkdirSync('/mnt/mock/adir');
+      }, /EEXIST/);
+   });
+
+   after(function(){
+      mounted.umount();
+   });
+});
+
