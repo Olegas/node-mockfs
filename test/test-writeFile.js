e551ec4b79be170e4b7bf1d6bd23d021852519e4
Oleg Elifantiev <oleg@elifantiev.ru>
2013-05-06 17:01:07 +0400

    writeFile[Sync] support

--- test/test-writeFile.js
+++ test/test-writeFile.js
@@ -17,20 +17,32 @@ describe("writeFile", function(){
 
    });
 
-   it("can create new files", function(){
+   it("can create new files", function(done){
 
       assert.equal(false, fs.existsSync('/mnt/mock/dir/file2'));
       fs.writeFileSync('/mnt/mock/dir/file2', "file2");
       assert.equal(true, fs.existsSync('/mnt/mock/dir/file2'));
       assert.equal("file2", fs.readFileSync('/mnt/mock/dir/file2').toString());
 
+      fs.writeFile('/mnt/mock/dir/file3', 'file3', function(e){
+         assert.equal(null, e);
+         assert.equal("file3", fs.readFileSync('/mnt/mock/dir/file3').toString());
+         done();
+      });
+
    });
 
-   it("can overwrite existing files", function(){
+   it("can overwrite existing files", function(done){
 
       assert.equal("qwerty", fs.readFileSync('/mnt/mock/file1').toString());
       fs.writeFileSync('/mnt/mock/file1', "file1");
       assert.equal("file1", fs.readFileSync('/mnt/mock/file1').toString());
+
+      fs.writeFile('/mnt/mock/file1', 'file1-async', function(e){
+         assert.equal(null, e);
+         assert.equal("file1-async", fs.readFileSync('/mnt/mock/file1').toString());
+         done();
+      });
 
    });
 
@@ -44,6 +56,12 @@ describe("writeFile", function(){
       assert.throws(function(){
          fs.writeFileSync('/mnt/mock/nonexist/file', 'willthrow');
       }, /ENOENT/);
+   });
+
+   it("throws ENOTDIR if trying to write to file in a non-directory", function(){
+      assert.throws(function(){
+         fs.writeFileSync('/mnt/mock/file1/file', 'willthrow');
+      }, /ENOTDIR/);
    });
 
    after(function(){
