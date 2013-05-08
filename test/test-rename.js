ffb7e98273ec2236abab95b7599715007f30c673
Oleg Elifantiev <oleg@elifantiev.ru>
2013-05-08 11:51:21 +0400

    rename - failing tests

--- test/test-rename.js
+++ test/test-rename.js
@@ -9,7 +9,11 @@ describe("rename", function(){
       mounted = mfs.mount({
          items: {
             'file': '',
-            'adir': {
+            'file2': '',
+            'dir2': {
+               items: {}
+            },
+            'dir': {
                items: {}
             }
          }
@@ -17,13 +21,20 @@ describe("rename", function(){
 
    });
 
-   it("rename a file", function(){
+   it("rename a file", function(done){
 
       assert.equal(true, fs.existsSync('/mnt/mock/file'));
       assert.equal(false, fs.existsSync('/mnt/mock/file-renamed'));
       fs.renameSync('/mnt/mock/file', '/mnt/mock/file-renamed');
       assert.equal(false, fs.existsSync('/mnt/mock/file'));
       assert.equal(true, fs.existsSync('/mnt/mock/file-renamed'));
+
+      fs.rename('/mnt/mock/file-renamed', '/mnt/mock/file-renamed-2', function(e) {
+         assert.equal(null, e);
+         assert.equal(false, fs.existsSync('/mnt/mock/file-renamed'));
+         assert.equal(true, fs.existsSync('/mnt/mock/file-renamed-2'));
+         done();
+      })
 
    });
 
@@ -35,6 +46,24 @@ describe("rename", function(){
       assert.equal(false, fs.existsSync('/mnt/mock/dir'));
       assert.equal(true, fs.existsSync('/mnt/mock/dir-renamed'));
 
+   });
+
+   it("throws EISDIR when trying to rename file to existing directory", function(){
+      assert.throws(function(){
+         fs.renameSync('/mnt/mock/file2', '/mnt/mock/dir2');
+      }, /EISDIR/);
+   });
+
+   it("throws ENOTDIR when trying to rename file to existing directory", function(){
+      assert.throws(function(){
+         fs.renameSync('/mnt/mock/dir2', '/mnt/mock/file2');
+      }, /ENOTDIR/);
+   });
+
+   it("throws ENOENT when trying to rename nonexisting file", function(){
+      assert.throws(function(){
+         fs.renameSync('/mnt/mock/non-exists', '/mnt/mock/file222');
+      }, /ENOENT/);
    });
 
    after(function(){
