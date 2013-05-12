081740e75d8fde07c7250063b438d7ce4591f6ea
Oleg Elifantiev <oleg@elifantiev.ru>
2013-05-12 15:30:42 +0400

    Tests for truncate*

--- test/test-fd.js
+++ test/test-fd.js
@@ -10,13 +10,16 @@ describe("fd* functions", function(){
          items: {
             'file': {
                content: new Buffer('qwerty')
-            }
+            },
+            'f5bytes': new Buffer("12345"),
+            'f10bytes': new Buffer("1234567890"),
+            'f20bytes': new Buffer("12345678901234567890")
          }
       }, '/mnt/mock');
 
    });
 
-   it("can open and close files", function(){
+   it("open returns an int greater than 0 file descriptor", function(){
 
       var fd = fs.openSync('/mnt/mock/file', 'r');
 
@@ -27,24 +30,82 @@ describe("fd* functions", function(){
 
    });
 
-   it("can read files by fd", function(){
+   describe("read(Sync)", function(){
 
-      var fd = fs.openSync('/mnt/mock/file', 'r');
-      var dst = new Buffer(3);
-      dst.fill('-');
-      var bytesRead = fs.readSync(fd, dst, 0, 3, 1);
+      it("can read files by fd", function(){
 
-      assert.equal(3, bytesRead);
-      assert.equal("wer", dst.toString());
+         var fd = fs.openSync('/mnt/mock/file', 'r');
+         var dst = new Buffer(3);
+         dst.fill('-');
+         var bytesRead = fs.readSync(fd, dst, 0, 3, 1);
 
-      // TODO check real
-      dst = new Buffer(3);
-      dst.fill('-');
-      bytesRead = fs.readSync(fd, dst, 0, 3, null);
-      assert.equal(2, bytesRead);
-      assert.equal("ty-", dst.toString());
+         assert.equal(3, bytesRead);
+         assert.equal("wer", dst.toString());
 
-      fs.closeSync(fd);
+         // TODO check real
+         dst = new Buffer(3);
+         dst.fill('-');
+         bytesRead = fs.readSync(fd, dst, 0, 3, null);
+         assert.equal(2, bytesRead);
+         assert.equal("ty-", dst.toString());
+
+         fs.closeSync(fd);
+      });
+
+   });
+
+   describe("ftruncate", function(){
+
+      it("Truncates a file content to specified number of bytes", function(done){
+         assert.equal(20, fs.statSync('/mnt/mock/f20bytes').size);
+         fs.open('/mnt/mock/f20bytes', 'r+', function(e, fd){
+            fs.truncate(fd, 15, function(e){
+               assert.equal(null, e);
+               fs.fstat(fd, function(e, stat){
+                  assert.equal(15, stat.size);
+                  fs.closeSync(fd);
+                  done();
+               });
+            });
+         })
+      });
+
+      it("Truncates a file content to zero bytes if no length specified", function(done){
+         assert.equal(10, fs.statSync('/mnt/mock/f10bytes').size);
+         fs.open('/mnt/mock/f10bytes', 'r+', function(e, fd){
+            fs.truncate(fd, function(e){
+               assert.equal(null, e);
+               assert.equal(0, fs.statSync('/mnt/mock/f10bytes').size);
+               fs.closeSync(fd);
+               done();
+            });
+         })
+      });
+
+      it("If truncate length is greated than file size, if is extended and filled with zeroes", function(done){
+         assert.equal(5, fs.statSync('/mnt/mock/f5bytes').size);
+         fs.open('/mnt/mock/f5bytes', 'r+', function(e, fd){
+            fs.truncate(fd, 15, function(e){
+               assert.equal(null, e);
+               assert.equal(15, fs.statSync('/mnt/mock/f5bytes').size);
+               var res = new Buffer(15);
+               fs.read(fd, res, 0, 15, null, function(e, bytesRead){
+                  assert.equal(null, e);
+                  assert.equal(15, bytesRead);
+                  assert.equal("12345\0\0\0\0\0\0\0\0\0\0", res.toString());
+                  fs.closeSync(fd);
+                  done();
+               });
+            });
+         })
+      });
+
+   });
+
+   it("if given a bad file descriptor, EBADF is thrown", function(){
+      assert.throws(function(){
+         fs.closeSync(-100);
+      }, /EBADF/);
    });
 
    after(function(){
