9117559ed082557daa0509f2a26dc3bfd88d00c6
Olegas <oleg@elifantiev.ru>
2013-05-13 15:49:48 +0400

    Test for unmounting with opened files

---
+++ test/test-mockfs.js
@@ -0,0 +1,32 @@
+var mfs = require('../'),
+   assert = require('assert'),
+   fs = require('fs'),
+   fdManager = require((process.env.COVER ? '../lib-cov/' : '../lib/') + 'fd-manager.js'),
+   mounted;
+
+describe("MockFS", function(){
+
+   it("when fs is unmounted with opened files - they are closed automatically", function(){
+
+      var _fs = new mfs({
+         items: {
+            file: '123'
+         }
+      });
+
+      _fs.mount('/mnt/mock');
+
+      assert.equal(true, fdManager.isSane());
+
+      fs.openSync('/mnt/mock/file', 'r');
+
+      assert.equal(false, fdManager.isSane());
+
+      _fs.umount();
+
+      assert.equal(true, fdManager.isSane());
+
+   });
+
+});
+
