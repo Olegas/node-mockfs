f8f59b05c2d7ed02e02fd7622e3839a8a97ee746
Oleg Elifantiev <oleg@elifantiev.ru>
2013-05-21 01:03:04 +0400

    access mode check routines

--- test/test-internals.js
+++ test/test-internals.js
@@ -4,74 +4,98 @@ var ac = require((process.env.COVER ? '../lib-cov/' : '../lib/') + 'access-contr
 describe("internals", function(){
    describe("access control utilities", function(){
 
-      it("can parse access mode flags from string", function(){
+      describe("mode parser", function(){
+         it("can parse access mode flags from string", function(){
 
-         assert.equal(parseInt('111101011', 2), ac.parseModeFlags('rwxr-x-wx'));
-         assert.equal(0, ac.parseModeFlags('---------'));
-         assert.equal(parseInt('0777', 8), ac.parseModeFlags('rwxrwxrwx'));
-         assert.equal(parseInt('0077', 8), ac.parseModeFlags('---rwxrwx'));
-         assert.equal(parseInt('0007', 8), ac.parseModeFlags('------rwx'));
+            assert.equal(parseInt('111101011', 2), ac.parseMode('rwxr-x-wx'));
+            assert.equal(0, ac.parseMode('---------'));
+            assert.equal(parseInt('0777', 8), ac.parseMode('rwxrwxrwx'));
+            assert.equal(parseInt('0077', 8), ac.parseMode('---rwxrwx'));
+            assert.equal(parseInt('0007', 8), ac.parseMode('------rwx'));
 
+         });
+
+         it("can parse access mode flags octal integer passed as string", function(){
+
+            assert.equal(parseInt('111101011', 2), ac.parseMode('0753'));
+            assert.equal(0, ac.parseMode('0000'));
+            assert.equal(parseInt('0777', 8), ac.parseMode('0777'));
+            assert.equal(parseInt('0077', 8), ac.parseMode('0077'));
+            assert.equal(parseInt('0007', 8), ac.parseMode('0007'));
+
+         });
+
+         it("numbers passed without modifications", function(){
+
+            assert.equal(parseInt('111101011', 2), ac.parseMode(0753));
+            assert.equal(0, ac.parseMode(0));
+            assert.equal(parseInt('0777', 8), ac.parseMode(0777));
+            assert.equal(parseInt('0077', 8), ac.parseMode(0077));
+            assert.equal(parseInt('0007', 8), ac.parseMode(0007));
+
+         });
+
+         it("incorrect symbols in string form throws RangeError", function(){
+
+            assert.throws(function(){
+               ac.parseMode('rwxrwxrwm');
+            }, /RangeError/);
+
+         });
+
+         it("incorrect symbols placing in string form throws RangeError", function(){
+
+            assert.throws(function(){
+               ac.parseMode('xwrrwxrwx');
+            }, /RangeError/);
+
+         });
+
+         it("passing some weird string throws TypeError (non string-mode, non octal number-as-string)", function(){
+            assert.throws(function(){
+               ac.parseMode('rwxrwxrwx-');
+            }, /TypeError/);
+
+            assert.throws(function(){
+               ac.parseMode('-rwxrwxrwx');
+            }, /TypeError/);
+
+            assert.throws(function(){
+               ac.parseMode('999');
+            }, /TypeError/);
+         });
+
+         it("passing non string, nor number throws TypeError", function(){
+            assert.throws(function(){
+               ac.parseMode(null);
+            }, /TypeError/);
+
+            assert.throws(function(){
+               ac.parseMode(new Date());
+            }, /TypeError/);
+         });
       });
 
-      it("can parse access mode flags octal integer passed as string", function(){
+      describe("access checker", function(){
 
-         assert.equal(parseInt('111101011', 2), ac.parseModeFlags('0753'));
-         assert.equal(0, ac.parseModeFlags('0000'));
-         assert.equal(parseInt('0777', 8), ac.parseModeFlags('0777'));
-         assert.equal(parseInt('0077', 8), ac.parseModeFlags('0077'));
-         assert.equal(parseInt('0007', 8), ac.parseModeFlags('0007'));
+         it("read", function(){
 
-      });
+            assert.equal(true, ac.canRead(1, 2, 'r--------', 1, 10));
+            assert.equal(false, ac.canRead(1, 2, 'r--------', 10, 10));
+            assert.equal(false, ac.canRead(1, 2, 'r--------', 10, 2));
+            assert.equal(true, ac.canRead(1, 2, 'r--r-----', 10, 2));
+            assert.equal(false, ac.canRead(1, 2, 'r--r---wx', 10, 20));
+            assert.equal(true, ac.canRead(1, 2, 'r--r--rwx', 10, 20));
+            assert.equal(true, ac.canRead(1, 2, 'r-----rwx', 10, 20));
+            assert.equal(true, ac.canRead(1, 2, '------rwx', 10, 20));
 
-      it("numbers passed without modifications", function(){
+            assert.equal(false, ac.canRead(1, 2, 'r--r-----'));
+            assert.equal(false, ac.canRead(1, 2, 'r--r-----', 2));
+            assert.equal(true, ac.canRead(1, 2, 'r--r-----', 0, 2));
+            assert.equal(true, ac.canRead(1, 2, '---r-----', 0, 2));
 
-         assert.equal(parseInt('111101011', 2), ac.parseModeFlags(0753));
-         assert.equal(0, ac.parseModeFlags(0));
-         assert.equal(parseInt('0777', 8), ac.parseModeFlags(0777));
-         assert.equal(parseInt('0077', 8), ac.parseModeFlags(0077));
-         assert.equal(parseInt('0007', 8), ac.parseModeFlags(0007));
+         })
 
-      });
-
-      it("incorrect symbols in string form throws RangeError", function(){
-
-         assert.throws(function(){
-            ac.parseModeFlags('rwxrwxrwm');
-         }, /RangeError/);
-
-      });
-
-      it("incorrect symbols placing in string form throws RangeError", function(){
-
-         assert.throws(function(){
-            ac.parseModeFlags('xwrrwxrwx');
-         }, /RangeError/);
-
-      });
-
-      it("passing some weird string throws TypeError (non string-mode, non octal number-as-string)", function(){
-         assert.throws(function(){
-            ac.parseModeFlags('rwxrwxrwx-');
-         }, /TypeError/);
-
-         assert.throws(function(){
-            ac.parseModeFlags('-rwxrwxrwx');
-         }, /TypeError/);
-
-         assert.throws(function(){
-            ac.parseModeFlags('999');
-         }, /TypeError/);
-      });
-
-      it("passing non string, nor number throws TypeError", function(){
-         assert.throws(function(){
-            ac.parseModeFlags(null);
-         }, /TypeError/);
-
-         assert.throws(function(){
-            ac.parseModeFlags(new Date());
-         }, /TypeError/);
       })
 
    });
