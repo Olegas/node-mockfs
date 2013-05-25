a70a6b556d6afd1c34b816f008e4932ce8f2ee02
Oleg Elifantiev <oleg@elifantiev.ru>
2013-05-25 23:50:58 +0400

    Tests for file/dir stat modifications implemented

--- test/test-stat-mod.js
+++ test/test-stat-mod.js
@@ -6,13 +6,15 @@ var mfs = require('../'),
 
 describe("file timestamps modification", function(){
 
-   before(function(){
+   beforeEach(function(){
       mounted = mfs.mount({
          time: now,
          items: {
             file: 'qwerty',
             dir: {
-               items: {}
+               items: {
+                  file: 'qwerty'
+               }
             }
          }
       }, '/mnt/mock');
@@ -20,30 +22,60 @@ describe("file timestamps modification", function(){
    });
 
    it("reading a file updates it's atime", function(){
-      throw new Error('WRITETHISTEST');
+      assert.equal(now, fs.statSync('/mnt/mock/file').atime);
+      fs.readFileSync('/mnt/mock/file');
+      assert.equal(true, now < fs.statSync('/mnt/mock/file').atime);
    });
 
    it("reading a file updates parent directory atime", function(){
-      throw new Error('WRITETHISTEST');
+      assert.equal(now, fs.statSync('/mnt/mock/dir/file').atime);
+      fs.readFileSync('/mnt/mock/dir/file');
+      assert.equal(true, now < fs.statSync('/mnt/mock/dir/file').atime);
    });
 
    it("writing to file updates parent directory atime", function(){
-      throw new Error('WRITETHISTEST');
+      assert.equal(now, fs.statSync('/mnt/mock/dir/file').atime);
+      fs.writeFileSync('/mnt/mock/dir/file', '123');
+      assert.equal(true, now < fs.statSync('/mnt/mock/dir').atime);
    });
 
    it("writing to file updates it's mtime and ctime", function(){
-      throw new Error('WRITETHISTEST');
+      assert.equal(now, fs.statSync('/mnt/mock/file').atime);
+      assert.equal(now, fs.statSync('/mnt/mock/file').mtime);
+      assert.equal(now, fs.statSync('/mnt/mock/file').ctime);
+      fs.writeFileSync('/mnt/mock/file', '123');
+      assert.equal(now, fs.statSync('/mnt/mock/file').atime);
+      assert.equal(true, now < fs.statSync('/mnt/mock/file').mtime);
+      assert.equal(true, now < fs.statSync('/mnt/mock/file').ctime);
    });
 
    it("creating a new file inside a directory updates it's mtime and ctime", function(){
-      throw new Error('WRITETHISTEST');
+      assert.equal(now, fs.statSync('/mnt/mock/dir').atime);
+      assert.equal(now, fs.statSync('/mnt/mock/dir').mtime);
+      assert.equal(now, fs.statSync('/mnt/mock/dir').ctime);
+      fs.writeFileSync('/mnt/mock/dir/file2', '123');
+      assert.equal(now, fs.statSync('/mnt/mock/dir').atime);
+      assert.equal(true, now < fs.statSync('/mnt/mock/dir').mtime);
+      assert.equal(true, now < fs.statSync('/mnt/mock/dir').ctime);
+   });
+
+   it("removing a file inside a directory updates it's mtime and ctime", function(){
+      assert.equal(now, fs.statSync('/mnt/mock/dir').atime);
+      assert.equal(now, fs.statSync('/mnt/mock/dir').mtime);
+      assert.equal(now, fs.statSync('/mnt/mock/dir').ctime);
+      fs.unlinkSync('/mnt/mock/dir/file');
+      assert.equal(now, fs.statSync('/mnt/mock/dir').atime);
+      assert.equal(true, now < fs.statSync('/mnt/mock/dir').mtime);
+      assert.equal(true, now < fs.statSync('/mnt/mock/dir').ctime);
    });
 
    it("renaming a file modifies file ctime", function(){
-      throw new Error("WRITETHISTEST");
+      assert.equal(now, fs.statSync('/mnt/mock/file').ctime);
+      fs.renameSync('/mnt/mock/file', '/mnt/mock/file2');
+      assert.equal(true, now < fs.statSync('/mnt/mock/file2').ctime);
    });
 
-   after(function(){
+   afterEach(function(){
       mounted.umount();
    });
 });
